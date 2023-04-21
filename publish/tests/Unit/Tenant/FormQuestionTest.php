a18a187facb915bbecd64e2184aac1a2470d9bd1
DDEV-Local User <nobody@example.com>
2023-04-21 21:25:31 +0000

    Auto Add

---
+++ publish/tests/Unit/Tenant/FormQuestionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Forms\Unit\Tenant;
+
+use PHPUnit\Framework\Attributes\Test;
+use ReesMcIvor\Forms\Models\Form;
+use Tests\TenantTestCase;
+
+class FormTest extends TenantTestCase {
+
+    #[Test]
+    public function a_form_can_be_created()
+    {
+        $form = Form::factory()->create();
+        $this->assertDatabaseHas('forms', $form->toArray());
+    }
+
+}
