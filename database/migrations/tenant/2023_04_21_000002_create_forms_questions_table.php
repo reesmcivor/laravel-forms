969fdf6eda01163bdb57de045befd30b48d824d7
DDEV-Local User <nobody@example.com>
2023-04-21 21:29:47 +0000

    Auto Add

---
+++ database/migrations/tenant/2023_04_21_000002_create_forms_questions_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('form_questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('form_questions');
+    }
+};
+
