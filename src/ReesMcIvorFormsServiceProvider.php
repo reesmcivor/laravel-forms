cad9a65ddf2d73abc22ffaff454a940ef1275254
DDEV-Local User <nobody@example.com>
2023-04-21 17:24:09 +0000

    Initial commit of the forms module

---
+++ src/ReesMcIvorFormsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ReesMcIvor\Forms;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReesMcIvorFormsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
