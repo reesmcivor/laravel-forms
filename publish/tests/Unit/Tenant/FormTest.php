063b7834eb6cc1003dcc53f14da79fe08f84e389
DDEV-Local User <nobody@example.com>
2023-04-21 17:37:53 +0000

    Auto Add

---
+++ publish/tests/Unit/Tenant/FormTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Forms\Unit\Tenant;
+
+use Tests\TenantTestCase;
+
+class FormTest extends TenantTestCase {
+
+    #[Test]
+    public function a_form_can_be_created()
+    {
+        $this->assertTrue(1, 1);
+    }
+
+}
