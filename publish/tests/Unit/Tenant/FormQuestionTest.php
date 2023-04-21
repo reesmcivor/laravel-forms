322177817cfe918e4b28630fd86127964971d156
DDEV-Local User <nobody@example.com>
2023-04-21 21:25:43 +0000

    Auto Add

--- publish/tests/Unit/Tenant/FormQuestionTest.php
+++ publish/tests/Unit/Tenant/FormQuestionTest.php
@@ -9,7 +9,7 @@ use Tests\TenantTestCase;
 class FormQuestionTest extends TenantTestCase {
 
     #[Test]
-    public function a_form_can_be_created()
+    public function a_form_can_have_a_given_question()
     {
         $form = Form::factory()->create();
         $this->assertDatabaseHas('forms', $form->toArray());
