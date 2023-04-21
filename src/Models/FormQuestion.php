4dc4ef4a4da60223a5640b03f31012773d1fd1ee
DDEV-Local User <nobody@example.com>
2023-04-21 21:26:49 +0000

    Auto Add

--- src/Models/FormQuestion.php
+++ src/Models/FormQuestion.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use ReesMcIvor\Forms\Database\Factories\FormFactory;
 
-class Form extends Model
+class FormQuestion extends Model
 {
     use HasFactory;
 
     protected static function newFactory()
     {
-        return FormFactory::new();
+        return FormQuestionFactory::new();
     }
 }
