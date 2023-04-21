f91d062a4dec0d76647ece79e1728d564cbc360a
DDEV-Local User <nobody@example.com>
2023-04-21 21:34:30 +0000

    Auto Add

--- publish/tests/Unit/Tenant/QuestionTest.php
+++ publish/tests/Unit/Tenant/QuestionTest.php
@@ -4,9 +4,10 @@ namespace Tests\Forms\Unit\Tenant;
 
 use PHPUnit\Framework\Attributes\Test;
 use ReesMcIvor\Forms\Models\Form;
+use ReesMcIvor\Forms\Models\Question;
 use Tests\TenantTestCase;
 
-class FormQuestionTest extends TenantTestCase {
+class QuestionTest extends TenantTestCase {
 
     #[Test]
     public function a_form_can_have_a_given_question()
