fabda3024e73951f1e9efe9de421913580ac6a1f
DDEV-Local User <nobody@example.com>
2023-04-21 21:25:49 +0000

    Auto Add

--- publish/tests/Unit/Tenant/FormQuestionTest.php
+++ publish/tests/Unit/Tenant/FormQuestionTest.php
@@ -12,6 +12,7 @@ class FormQuestionTest extends TenantTestCase {
     public function a_form_can_have_a_given_question()
     {
         $form = Form::factory()->create();
+        $question = Question::factory()->create();
         $this->assertDatabaseHas('forms', $form->toArray());
     }
 
