02d6a57074d74f09989824bcb6a87028929d5778
DDEV-Local User <nobody@example.com>
2023-04-21 21:28:02 +0000

    Auto Add

--- database/migrations/tenant/2023_04_21_000001_create_form_question_table.php
+++ database/migrations/tenant/2023_04_21_000001_create_form_question_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('forms', function (Blueprint $table) {
+        Schema::create('form_questions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
