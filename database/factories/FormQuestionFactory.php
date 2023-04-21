1ca5140d7a460b79169f44c1655d30f284086e94
DDEV-Local User <nobody@example.com>
2023-04-21 21:27:32 +0000

    Auto Add

--- database/factories/FormQuestionFactory.php
+++ database/factories/FormQuestionFactory.php
@@ -4,10 +4,11 @@ namespace ReesMcIvor\Forms\Database\Factories;
 
 use ReesMcIvor\Forms\Models\Form;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use ReesMcIvor\Forms\Models\FormQuestion;
 
 class FormQuestionFactory extends Factory
 {
-    protected $model = Form::class;
+    protected $model = FormQuestion::class;
 
     public function definition()
     {
