415885637a4879f76b800ebf0a9d68e19dfdee2f
DDEV-Local User <nobody@example.com>
2023-04-21 21:25:35 +0000

    Auto Add

--- publish/tests/Unit/Tenant/FormQuestionTest.php
+++ publish/tests/Unit/Tenant/FormQuestionTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\Attributes\Test;
 use ReesMcIvor\Forms\Models\Form;
 use Tests\TenantTestCase;
 
-class FormTest extends TenantTestCase {
+class FormQuestionTest extends TenantTestCase {
 
     #[Test]
     public function a_form_can_be_created()
