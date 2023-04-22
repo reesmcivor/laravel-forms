6208407895e248bfe938a412e44754aa35e26b6a
DDEV-Local User <nobody@example.com>
2023-04-22 21:10:58 +0000

    Auto Add

--- src/FormsPackageServiceProvider.php
+++ src/FormsPackageServiceProvider.php
@@ -20,7 +20,7 @@ class FormsPackageServiceProvider extends ServiceProvider
         }
 
         $this->loadRoutesFrom(__DIR__.'/routes/tenant.php');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'forms');
+        $this->loadViewsFrom(__DIR__.'/resources/sviews', 'forms');
     }
 
     public function map()
