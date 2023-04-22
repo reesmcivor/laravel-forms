f115a4fc47b52552fe56ca795bfa2446970fcd35
DDEV-Local User <nobody@example.com>
2023-04-22 21:30:41 +0000

    Auto Add

---
+++ src/resources/views/forms/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.tenant', ['title' => 'Forms'])
+
+@section('content')
+
+    <div class="">
+        <div class="max-w-7xl mx-auto">
+            <x-button class="px-5" as="a" href="{{ route('tenant.forms.create') }}">New form</x-button>
+            <div class="">
+                @foreach($forms as $form)
+                    {{ $form->name }}
+                    <a href="{{ route('tenant.forms.edit', $form) }}">Edit</a>
+                    <a href="{{ route('tenant.forms.destroy', $form) }}">Delete</a>
+                @endforeach
+
+                {{ $forms->links() }}
+            </div>
+        </div>
+    </div>
+
+@endsection
