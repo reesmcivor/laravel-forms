fa2baad814362d7d48ff90661423bc5893a1f0c7
DDEV-Local User <nobody@example.com>
2023-04-22 05:35:15 +0000

    Auto Add

--- database/migrations/tenant/2023_04_21_000003_create_answerables_table.php
+++ database/migrations/tenant/2023_04_21_000003_create_answerables_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('form_question', function (Blueprint $table) {
+        Schema::create('answerables', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Question::class);
             $table->foreignIdFor(Form::class);
