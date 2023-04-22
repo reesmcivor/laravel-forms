6f95080c3e17123d74a35f6bf7809ab84c7015e1
DDEV-Local User <nobody@example.com>
2023-04-22 05:45:07 +0000

    Auto Add

--- src/FormsPackageServiceProvider.php
+++ src/FormsPackageServiceProvider.php
@@ -11,7 +11,7 @@ class FormsPackageServiceProvider extends ServiceProvider
         if($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../database/migrations/tenant' => database_path('migrations/tenant'),
-                __DIR__ . '/../database/factories' => database_path('factories'),
+                //__DIR__ . '/../database/factories' => database_path('factories'),
                 __DIR__ . '/../publish/tests' => base_path('tests/Forms'),
             ], 'reesmcivor-forms');
         }
