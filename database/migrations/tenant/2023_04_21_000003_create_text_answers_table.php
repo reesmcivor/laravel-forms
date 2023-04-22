dbc5fbe1f99e82c788d11b247fc7b1654c7801ed
DDEV-Local User <nobody@example.com>
2023-04-22 05:49:05 +0000

    Auto Add

--- database/migrations/tenant/2023_04_21_000003_create_text_answers_table.php
+++ database/migrations/tenant/2023_04_21_000003_create_text_answers_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('choice_answers', function (Blueprint $table) {
+        Schema::create('text_answers', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Question::class);
             $table->foreignIdFor(Form::class);
