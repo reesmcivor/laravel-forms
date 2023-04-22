7f847dedfcc8f9034591834fcf1353ae2c55cda5
DDEV-Local User <nobody@example.com>
2023-04-22 17:37:25 +0000

    Auto Add

--- database/migrations/tenant/2023_04_21_000002_create_questions_table.php
+++ database/migrations/tenant/2023_04_21_000002_create_questions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('question');
-            $table->enum('type', ['text', 'choice', 'date', 'number']);
+            $table->enum('type', ['text', 'select', 'date', 'number']);
             $table->timestamps();
         });
     }
