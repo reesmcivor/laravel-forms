de938f8a34ec048a1ea57c26c3fa803cf4ea1045
DDEV-Local User <nobody@example.com>
2023-04-22 05:34:52 +0000

    Auto Add

---
+++ database/migrations/tenant/2023_04_21_000003_create_answerables_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use ReesMcIvor\Forms\Models\Form;
+use ReesMcIvor\Forms\Models\Question;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('form_question', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Question::class);
+            $table->foreignIdFor(Form::class);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('form_question');
+    }
+};
+
