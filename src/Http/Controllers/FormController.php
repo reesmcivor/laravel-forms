a69319ee7966b5bda5e3ebf017493153de52ebfd
DDEV-Local User <nobody@example.com>
2023-04-22 21:27:25 +0000

    Auto Add

--- src/Http/Controllers/FormController.php
+++ src/Http/Controllers/FormController.php
@@ -4,11 +4,13 @@ namespace ReesMcIvor\Forms\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use ReesMcIvor\Forms\Models\Form;
 
 class FormController extends Controller
 {
     public function index()
     {
+        $forms = Form::paginate(10);
         return view('forms::forms.index');
     }
 }
