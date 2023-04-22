edad0e7362ebddfe4959aabb34f231544d650645
DDEV-Local User <nobody@example.com>
2023-04-22 10:50:39 +0000

    Auto Add

--- src/Models/ChoiceAnswer.php
+++ src/Models/ChoiceAnswer.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use ReesMcIvor\Forms\Database\Factories\TextAnswerFactory;
 
-class TextAnswer extends Model
+class ChoiceAnswer extends Model
 {
     use HasFactory;
 
