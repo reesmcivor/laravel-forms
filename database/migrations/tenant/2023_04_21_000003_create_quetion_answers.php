82b1ed60ba6609bf03e03cdf297234f0786a8367
DDEV-Local User <nobody@example.com>
2023-04-22 06:17:54 +0000

    Auto Add

--- database/migrations/tenant/2023_04_21_000003_create_quetion_answers.php
+++ database/migrations/tenant/2023_04_21_000003_create_quetion_answers.php
@@ -21,7 +21,7 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('choice_answers');
+        Schema::dropIfExists('text_answers');
     }
 };
 
