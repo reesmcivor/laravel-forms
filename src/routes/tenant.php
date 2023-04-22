626a2e69846c2187454081751ff4666521f773ec
DDEV-Local User <nobody@example.com>
2023-04-22 18:36:34 +0000

    Auto Add

--- src/routes/tenant.php
+++ src/routes/tenant.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['tenant'])->group(function () {
-    // Your tenant-specific routes go here
+
+    Route::get('forms', 'FormsController@index')->name('forms.index');
+
 });
