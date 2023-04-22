31d3a6aee681cacc13473001bee9354529bbe533
DDEV-Local User <nobody@example.com>
2023-04-22 06:17:22 +0000

    Auto Add

---
+++ database/migrations/tenant/2023_04_21_000003_create_quetion_answers.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use ReesMcIvor\Forms\Models\Form;
+use ReesMcIvor\Forms\Models\Question;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('text_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Question::class);
+            $table->text('answer');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('choice_answers');
+    }
+};
+
