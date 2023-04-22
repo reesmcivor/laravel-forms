e2d693282f97bf65847007bc7e7c4b757c2a2109
DDEV-Local User <nobody@example.com>
2023-04-22 21:15:33 +0000

    Auto Add

--- src/resources/views/forms/index.blade.php
+++ src/resources/views/forms/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.tenant', ['title' => 'Blog'])
+@extends('layouts.tenant', ['title' => 'Forms'])
 
 @section('content')
 
@@ -6,7 +6,7 @@
         <div class="max-w-7xl mx-auto">
             <x-button class="px-5" as="a" href="{{ route('tenant.posts.create') }}">New post</x-button>
             <div class="">
-                
+
             </div>
         </div>
     </div>
