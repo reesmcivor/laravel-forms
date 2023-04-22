e488ccda2c6f18fae541af8576847242a4f9671e
DDEV-Local User <nobody@example.com>
2023-04-22 10:51:22 +0000

    Auto Add

--- database/factories/ChoiceAnswerFactory.php
+++ database/factories/ChoiceAnswerFactory.php
@@ -5,14 +5,14 @@ namespace ReesMcIvor\Forms\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use ReesMcIvor\Forms\Models\TextAnswer;
 
-class TextAnswerFactory extends Factory
+class ChoiceAnswerFactory extends Factory
 {
     protected $model = TextAnswer::class;
 
     public function definition()
     {
         return [
-            'answer' => $this->faker->name,
+            'choice_id' => Choice::factory()->create(),
         ];
     }
 }
