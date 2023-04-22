ef1dc998169638e7d033f719bf27d7c0ce652ed8
DDEV-Local User <nobody@example.com>
2023-04-22 18:36:28 +0000

    Auto Add

---
+++ src/routes/tenant.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register tenant-specific routes for your module. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "tenant" middleware group. Now create something great!
+|
+*/
+
+Route::middleware(['tenant'])->group(function () {
+    // Your tenant-specific routes go here
+});
