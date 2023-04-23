28102b8c7d3c665367c7e60306a7e96b40fccbf7
DDEV-Local User <nobody@example.com>
2023-04-23 17:41:11 +0000

    Auto Add

--- src/resources/views/forms/index.blade.php
+++ src/resources/views/forms/index.blade.php
@@ -20,7 +20,7 @@
                         <form method="POST" action="{{ route('tenant.forms.destroy', $form->id) }}">
                             @csrf
                             @method('DELETE')
-                            <x-button class="px-5 bg-red-400" as="button" type="submit">Delete</x-button>
+                            <x-button variant="secondary" type="submit" href="{{ route('tenant.forms.index') }}">Cancel</x-button>
                         </form>
                     </div>
                 </div>
