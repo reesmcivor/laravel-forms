cdddc0a4663037f9c84a5643d0a2ebed5401ee91
DDEV-Local User <nobody@example.com>
2023-04-23 17:31:24 +0000

    Auto Add

--- src/resources/views/forms/index.blade.php
+++ src/resources/views/forms/index.blade.php
@@ -8,8 +8,10 @@
 
                 @foreach($forms as $form)
                     <div class="block mt-8 rounded-lg shadow overflow-hidden">
+                        <div class="bg-white">
                         {{ $form->name }}
                         <a href="{{ route('tenant.forms.edit', $form) }}">Edit</a>
+                        </div>
                     <div>
                 @endforeach
 
