50fa255e710a4513191bd507fda4bc7e1cb2d0d3
DDEV-Local User <nobody@example.com>
2023-04-23 18:51:35 +0000

    Auto Add

--- src/Http/Controllers/FormController.php
+++ src/Http/Controllers/FormController.php
@@ -101,7 +101,7 @@ class FormController extends Controller
         {
             $question = Question::find($questionId);
             if($question->type == "select") {
-                ChoiceAnswer::create([ "question_id" => $question->id,  "choice_id" => $choice->id]);
+                ChoiceAnswer::create([ "question_id" => $question->id,  "choice_id" => $questionAnswer->id]);
                 QuestionAnswer::create([
                     'form_entry_id' => $formEntry->id,
                     'question_id' => $question->id,
