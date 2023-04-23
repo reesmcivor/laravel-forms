5ebb37edd3099b334cd29da3ee9127b54a82917f
DDEV-Local User <nobody@example.com>
2023-04-23 17:35:27 +0000

    Auto Add

--- src/routes/tenant.php
+++ src/routes/tenant.php
@@ -24,12 +24,6 @@ Route::middleware('tenant', PreventAccessFromCentralDomains::class)->name('tenan
     Route::middleware(['auth', CheckSubscription::class])->group(function () {
         Route::middleware(OwnerOnly::class)->group(function () {
             Route::resource('forms', Controllers\FormController::class);
-
-            //Route::get('forms', [Controllers\FormController::class, 'index'])->name('forms.index');
-            //Route::post('forms', [Controllers\FormController::class, 'store'])->name('forms.store');
-            //Route::get('forms/create', [Controllers\FormController::class, 'create'])->name('forms.create');
-            //Route::get('forms/edit/{id}', [Controllers\FormController::class, 'create'])->name('forms.edit');
-
         });
     });
 
