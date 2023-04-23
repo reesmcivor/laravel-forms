65ceb9a8df50d39440c150760f3259f35f4ab8e3
DDEV-Local User <nobody@example.com>
2023-04-23 17:30:36 +0000

    Auto Add

--- src/resources/views/forms/index.blade.php
+++ src/resources/views/forms/index.blade.php
@@ -5,7 +5,7 @@
     <div class="">
         <div class="max-w-7xl mx-auto">
             <x-button class="px-5" as="a" href="{{ route('tenant.forms.create') }}">New form</x-button>
-            <div class="">
+            <div class="bg-white my-10">
                 @foreach($forms as $form)
                     {{ $form->name }}
                     <a href="{{ route('tenant.forms.edit', $form) }}">Edit</a>
