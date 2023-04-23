f8d48a1f176b7d432af863f8ab3584616071599c
DDEV-Local User <nobody@example.com>
2023-04-23 17:30:18 +0000

    Auto Add

--- src/routes/tenant.php
+++ src/routes/tenant.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\CheckSubscription;
+use App\Http\Middleware\OwnerOnly;
 use Illuminate\Support\Facades\Route;
 
 use ReesMcIvor\Forms\Http\Controllers as Controllers;
