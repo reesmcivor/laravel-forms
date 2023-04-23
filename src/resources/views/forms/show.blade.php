8e1ed8502badcb9d920b1fe5ab27ddc3fb4aed2b
DDEV-Local User <nobody@example.com>
2023-04-23 18:39:09 +0000

    Auto Add

--- src/resources/views/forms/show.blade.php
+++ src/resources/views/forms/show.blade.php
@@ -24,7 +24,7 @@
 
 
                             @if($question->type == 'text')
-                                <input type="text" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 rounded-md shadow-sm mt-1 block w-full @error('description') border-red-500 @enderror">
+                                <input name="question[{{ $question->id }}] type="text" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 rounded-md shadow-sm mt-1 block w-full @error('description') border-red-500 @enderror">
                             @endif
 
                             @if($question->type == "select")
