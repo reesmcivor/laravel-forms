2e5faf6f809b1ad4aacde2c71906715a3d870d86
DDEV-Local User <nobody@example.com>
2023-04-23 17:51:45 +0000

    Auto Add

--- src/Http/Controllers/FormController.php
+++ src/Http/Controllers/FormController.php
@@ -31,6 +31,7 @@ class FormController extends Controller
     public function show(Form $form)
     {
 
+        Question::all()->query()->delete();
         $form->questions()->attach(
             Question::create([
                 'type' => 'text',
