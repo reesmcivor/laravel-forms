c9beed660594503bf9a71a165da9c49390bd5727
DDEV-Local User <nobody@example.com>
2023-04-23 17:46:20 +0000

    Auto Add

--- src/resources/views/forms/show.blade.php
+++ src/resources/views/forms/show.blade.php
@@ -5,6 +5,14 @@
     <div class="">
         <div class="max-w-7xl mx-auto">
 
+            <h1 class="text-2xl font-semibold text-gray-900">
+                {{ $form->name }}
+            </h1>
+
+            <div class="mt-3 text-base text-gray-500">
+                {!! $form->description !!}
+            </div>
+            
 
         </div>
     </div>
